cbd6cae7a7f3928fd5714a4494a7243e5b0857a1
Rohithkp.me@gmail.com <Rohithkp.me@gmail.com>
2015-07-30 14:53:30 +0530

    WodoTextEditor Added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 import os
 from app import app
-from flask import render_template,request
+from flask import render_template,request, send_from_directory
 import sec
 import glob
 import time
@@ -28,3 +28,8 @@ def gen():
 @app.route('/sample/<templatename>')
 def ren(templatename):
     return render_template('ren.html',temp=temp,templatename=templatename)
+
+@app.route('/wodo/<path:path>')
+def send_js(path):
+    wpath = os.path.join(root,'./static/wodo/')
+    return send_from_directory(wpath, path)
