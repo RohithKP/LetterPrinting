ec8b2490feec49ba7b8eea63acdf5f273dd4aba3
Rohithkp.me@gmail.com <Rohithkp.me@gmail.com>
2015-08-04 15:56:32 +0530

    upload

--- app/views.py
+++ app/views.py
@@ -1,13 +1,19 @@
 import os
 from app import app
-from flask import render_template,request, send_from_directory,url_for
+from flask import render_template,request, send_from_directory,url_for,redirect
 import sec
 import glob
 import time
 import random
+from werkzeug import secure_filename
 
 root =  os.path.dirname(__file__)
 path = os.path.join(root,'./static/odt/')
+
+UPLOAD_FOLDER = path
+ALLOWED_EXTENSIONS = set(['odt'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 #temp = glob.glob(path)
 temp = os.listdir(path)
 
@@ -45,3 +51,30 @@ def viewer():
 @app.route('/temp/')
 def func():
     return render_template('vjs.html')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
